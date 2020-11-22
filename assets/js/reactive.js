878c7e7a306f5b1cea1461152996fb06dbe2b3b6
natsume0718 <moto718program@gmail.com>
2020-11-23 01:35:16 +0900

    複数オブジェクトへの対応

--- assets/js/reactive.js
+++ assets/js/reactive.js
@@ -1,26 +1,32 @@
 "use strict";
 
 let activeEffect = null;
+const mapping = new WeakMap();
 
 function effect(fn) {
   activeEffect = fn;
+  activeEffect();
 }
 
-function trigger() {
-  if (activeEffect !== null) {
-    activeEffect();
-  }
+function trigger(target) {
+  const effect = mapping.get(target);
+  effect();
+}
+
+function track(target) {
+  mapping.set(target, activeEffect);
 }
 
 function reactive(target) {
   const handler = {
     get(target, key, reciever) {
       const ref = Reflect.get(target, key, reciever);
+      track(target);
       return ref;
     },
     set(target, key, value, reciever) {
       const ref = Reflect.set(target, key, value, reciever);
-      trigger();
+      trigger(target);
       return ref;
     },
   };
