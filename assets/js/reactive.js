dd5745e5b3cc5bb85603b5b000f83cca03b1ba60
natsume0718 <moto718program@gmail.com>
2020-11-23 12:29:07 +0900

    オブジェクトのプロパティの特定を追加

--- assets/js/reactive.js
+++ assets/js/reactive.js
@@ -3,30 +3,57 @@
 let activeEffect = null;
 const mapping = new WeakMap();
 
+/**
+ * メソッドの格納
+ * @param {function} fn
+ */
 function effect(fn) {
   activeEffect = fn;
   activeEffect();
 }
 
-function trigger(target) {
-  const effect = mapping.get(target);
+/**
+ * 登録されたメソッドの実行
+ * @param {object} target
+ * @param {string} key
+ */
+function trigger(target, key) {
+  const dependencyMap = mapping.get(target);
+  if (!dependencyMap) {
+    return;
+  }
+  const effect = dependencyMap.get(key);
   effect();
 }
 
-function track(target) {
-  mapping.set(target, activeEffect);
+/**
+ * 依存関係の登録
+ * @param {object} target
+ * @param {string} key
+ */
+function track(target, key) {
+  let dependencyMap = mapping.get(target);
+  if (!dependencyMap) {
+    dependencyMap = new Map();
+    mapping.set(target, dependencyMap);
+  }
+  dependencyMap.set(key, activeEffect);
 }
 
+/**
+ * reactivityを有効化するオブジェクトを登録と、getter、setterの定義
+ * @param {object} target
+ */
 function reactive(target) {
   const handler = {
     get(target, key, reciever) {
       const ref = Reflect.get(target, key, reciever);
-      track(target);
+      track(target, key);
       return ref;
     },
     set(target, key, value, reciever) {
       const ref = Reflect.set(target, key, value, reciever);
-      trigger(target);
+      trigger(target, key);
       return ref;
     },
   };
--- reactive.ex.js
+++ reactive.ex.js
@@ -1,20 +1,20 @@
 import { effect, reactive } from "./assets/js/reactive.js";
 
 let num = document.querySelector("#js-view").textContent;
-const obj = reactive({ a: num });
+const obj = reactive({ counter: num });
 
 document
   .querySelector("#js-reactiveIncrement")
   .addEventListener("click", () => {
-    obj.a++;
+    obj.counter++;
   });
 document
   .querySelector("#js-reactiveDecrement")
   .addEventListener("click", () => {
-    obj.a--;
+    obj.counter--;
   });
 
 effect(() => {
-  document.querySelector("#js-view").textContent = obj.a;
-  document.querySelector("#js-reactiveResult").textContent = obj.a * 2;
+  document.querySelector("#js-view").textContent = obj.counter;
+  document.querySelector("#js-reactiveResult").textContent = obj.counter * 2;
 });
